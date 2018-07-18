dffdb65cc0b2eac61be9c2194a6f80bd0248b5c2
Aurele Sarrail <aurele.sarrail@gmail.com>
2018-07-18 20:09:27 +0200

    Post getters are ready and some setters

--- Modele.php
+++ Modele.php
@@ -4,18 +4,77 @@ class post
 	
 	// d'abord les attributs
 
-	private $Post_Id;
-	private $User_Id;
+	private $PostId;
+	private $UserId;
 	private $Title;
 	private $Head;
 	private $Image;
 	private $Content;
-	private $Last_Modif;
-	private $Creat_Date;
+	private $LastModif;
+	private $CreatDate;
 
 	// ensuite les méthodes
+	// je commence par les getters
 
-	function
+	public function getPostId
+	{
+		return $this->_PostId;
+	}
+
+	public function getUserId
+	{
+		return $this->_UserId;
+	}
+
+	public function getTitle
+	{
+		return $this->_Title;
+	}
+
+	public function getHead
+	{
+		return $this->_Head;
+	}
+
+	public function getImage
+	{
+		return $this->_Image;
+	}
+
+	public function getContent
+	{
+		return $this->_Content;
+	}
+
+	public function getLastModif
+	{
+		return $this->_LastModif;
+	}
+
+	public function getCreatDate
+	{
+		return $this->_CreatDate;
+	}
+
+	// on fait ensuite les setters
+
+	public function setPostId($PostId)
+	{
+		$this->_PostId = (int) $PostId;
+	}
+
+	public function setUserId($UserId)
+	{
+		$this->_UserId = (int) $UserId;
+	}
+
+	public function setTitle($Title)
+	{
+		if(is_string($Title))
+		{
+			$this->_Title = $Title;
+		}
+	}
 
 
 }
