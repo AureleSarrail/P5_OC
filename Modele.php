cc0c962a1bbf9cbebab578b67edd10156c3e728d
Aurele Sarrail <aurele.sarrail@gmail.com>
2018-07-18 19:50:04 +0200

    j'ai ajouté le fichier de classe

---
+++ Modele.php
@@ -0,0 +1,21 @@
+<?
+class post
+{
+	
+	// d'abord les attributs
+
+	private $Post_Id;
+	private $User_Id;
+	private $Title;
+	private $Head;
+	private $Image;
+	private $Content;
+	private $Last_Modif;
+	private $Creat_Date;
+
+	// ensuite les méthodes
+
+	function
+
+
+}
