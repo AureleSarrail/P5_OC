c7ce642dff99afd28d4adb0743249aab341cc03c
Aurele Sarrail <aurele.sarrail@gmail.com>
2018-07-18 20:49:27 +0200

    Class model - Creation and add DbConnect and GetPOsts functions

---
+++ Modele.php
@@ -0,0 +1,17 @@
+<?
+
+class model
+{
+	private function DbConnect()
+	{
+		$Db = new PDO('mysql:host=localhost;dbname=Blog_asdev;charset=utf8','root','',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+		return $Db;
+	}
+
+	public function getPosts()
+	{
+		$Db = $this->DbConnect();
+		$Posts = $Db->query('select * from post order by postid limit 10');
+		return $Posts;
+	}
+}
