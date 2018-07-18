c0df4e137bf7a3830d5f8f9c45eaa5f08b0fb072
Aurele Sarrail <aurele.sarrail@gmail.com>
2018-07-18 22:02:54 +0200

    some minor modification to template

--- template.php
+++ template.php
@@ -2,13 +2,13 @@
 <html>
 <head>
 	<meta charset="UTF-8">
-	<title><?php echo($ViewTitle); ?></title>
-	<link rel="stylesheet" href="css/bootstrap.css">
+	<title><?= $ViewTitle ?></title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<link rel="stylesheet" href="style.css">
 </head>
 <body>
 	<div class="container-fluid">
-		<header id="header">
+		<header class="col-lg-12" id="header">
 			<h3 class="col-lg-3">Blog AS-DEV</h3>
 			<ul>
 				<li class="col-lg-2"><a href="#acceuil">Acceuil</a></li>
@@ -17,14 +17,14 @@
 			</ul>
 		</header>
 
-		<?php echo($ViewContent); ?>
+		<?= $ViewContent ?>
 
 		<footer class="col-lg-12">
-			<h3 class="col-lg-3">Blog AS-DEV</h3>
 			<ul>
 				<li class="col-lg-2"><a href="#acceuil">Acceuil</a></li>
 				<li class="col-lg-2"><a href="#Posts">Les Posts</a></li>
 				<li class="col-lg-2"><a href="#contact">Contactez nous</a></li>
+				<li class="col-lg-push-3 col-lg-3"><a href="#connection">Connexion</a></li>
 			</ul>
 		</footer>
 	</div>
