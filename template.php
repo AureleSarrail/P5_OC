aea4244bf577ff7119def785649fc3f343b957b4
Aurele Sarrail <aurele.sarrail@gmail.com>
2018-07-24 12:01:40 +0200

    template page creation

--- template.php
+++ template.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta charset="UTF-8">
-	<title><?= $ViewTitle ?></title>
+	<title>page test</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<link rel="stylesheet" href="style.css">
 </head>
@@ -11,21 +11,23 @@
 		<header class="col-lg-12" id="header">
 			<h3 class="col-lg-3">Blog AS-DEV</h3>
 			<ul>
-				<li class="col-lg-2"><a href="#acceuil">Acceuil</a></li>
-				<li class="col-lg-2"><a href="#Posts">Les Posts</a></li>
-				<li class="col-lg-2"><a href="#contact">Contactez nous</a></li>
+				<li class="col-lg-2"><button  type="button" class="btn btn-link"><a href="#acceuil">Acceuil</a></button></li>
+				<li class="col-lg-2"><button  type="button" class="btn btn-link"><a href="#Posts">Les Posts</a></button></li>
+				<li class="col-lg-2"><button  type="button" class="btn btn-link"><a href="#contact">Contactez nous</a></button></li>
 			</ul>
 		</header>
 
-		<?= $ViewContent ?>
+		
 
-		<footer class="col-lg-12">
-			<ul>
-				<li class="col-lg-2"><a href="#acceuil">Acceuil</a></li>
-				<li class="col-lg-2"><a href="#Posts">Les Posts</a></li>
-				<li class="col-lg-2"><a href="#contact">Contactez nous</a></li>
-				<li class="col-lg-push-3 col-lg-3"><a href="#connection">Connexion</a></li>
-			</ul>
+		<footer class="footer">
+			<div class="container">
+				<ul>
+					<li class="col-lg-2"><a href="#acceuil">Acceuil</a></li>
+					<li class="col-lg-2"><a href="#Posts">Les Posts</a></li>
+					<li class="col-lg-2"><a href="#contact">Contactez nous</a></li>
+					<li class="col-lg-push-3 col-lg-3"><a href="#connection">Connexion</a></li>
+				</ul>
+			</div>
 		</footer>
 	</div>
 </body>
