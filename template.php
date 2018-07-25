cae644da307e3f03ec30813b96d6ce625e05852f
Aurele Sarrail <aurele.sarrail@gmail.com>
2018-07-25 11:39:41 +0200

    template is finish - try to get work in mvc add font add images and some css

--- template.php
+++ template.php
@@ -3,32 +3,81 @@
 <head>
 	<meta charset="UTF-8">
 	<title>page test</title>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 </head>
-<body>
-	<div class="container-fluid">
-		<header class="col-lg-12" id="header">
-			<h3 class="col-lg-3">Blog AS-DEV</h3>
-			<ul>
-				<li class="col-lg-2"><button  type="button" class="btn btn-link"><a href="#acceuil">Acceuil</a></button></li>
-				<li class="col-lg-2"><button  type="button" class="btn btn-link"><a href="#Posts">Les Posts</a></button></li>
-				<li class="col-lg-2"><button  type="button" class="btn btn-link"><a href="#contact">Contactez nous</a></button></li>
-			</ul>
-		</header>
 
-		
-
-		<footer class="footer">
-			<div class="container">
-				<ul>
-					<li class="col-lg-2"><a href="#acceuil">Acceuil</a></li>
-					<li class="col-lg-2"><a href="#Posts">Les Posts</a></li>
-					<li class="col-lg-2"><a href="#contact">Contactez nous</a></li>
-					<li class="col-lg-push-3 col-lg-3"><a href="#connection">Connexion</a></li>
-				</ul>
+<div class="container-fluid" id="master_div">
+	<body>
+		<div id="wrapper">
+			<div id="header_global" class="row">
+				<header class="col-lg-12">
+					<div class="row">
+						<h1 class="col-lg-3" div id="logo">Blog AS-DEV</h1>
+						<ul id="nav_head" class=" list-unstyled list-inline col-lg-push-3 col-lg-6">
+							<li class="list-inline-item"><button  type="button" class="btn btn-link"><a href="#acceuil">Acceuil</a></button></li>
+							<li class="list-inline-item"><button  type="button" class="btn btn-link"><a href="#Posts">Les Posts</a></button></li>
+							<li class="list-inline-item"><button  type="button" class="btn btn-link"><a href="#contact">Contactez nous</a></button></li>
+						</ul>
+					</div>
+				</header>
 			</div>
-		</footer>
-	</div>
-</body>
+			<!-- Début du contenu à rendre MVC ensuite -->
+
+			<div id="content_block">
+				<div class="row" id="title_bloc">
+					<div class="col-lg-offset-2 col-lg-7">
+						<img src="images/photo.jpeg" alt="photo_de_moi" class="col-lg-1">
+						<div class="col-lg-5">
+							<p class="col-lg-12" id="name">Aurèle Sarrail</p>
+							<p id="accroche" class="col-lg-12">Le développeur de votre future solution Web !</p>
+						</div>	
+					</div>	
+				</div>
+
+				<div class="row" id="pres_bloc">
+					<div class="col-lg-push-2 col-lg-8">
+						<h2>Bienvenue dans ce Blog</h2>
+						<br>
+						<p>Dans ce Blog, j'essaierai de vous faire suivre mes projets, mes avancements.</p>
+						<br>
+						<p>Si vous souhaitez parcourir mon Cv, il est en téléchargement en cliquant <a href="files/cv.pdf">ici</a>.</p>
+						<br>
+						<p>Ici vous pourrez me suivre, voir mes derniers articles postés.</p>
+						<br>
+						<p>Vous pourrez également me contacter via le formulaire de contact ou les réseaux sociaux, tant sur le plan professionnel que personnel.</p>
+						<br>
+						<p>Bonne visite à tous !</p>
+						<br>
+					</div>
+				</div>
+
+				<div class="row" id="social_media">
+					<div class="col-lg-push-2 col-lg-8">
+						<h2>Mes reseaux sociaux</h2>
+						<br>
+						<p>Ici vous pouvez retrouver les liens vers mes différents réseaux sociaux</p>
+						<br>
+						<ul>
+							<li><a href="https://www.facebook.com/aurele.sarrail">Facebook</a></li>
+							<li><a href="https://twitter.com/">Twitter</a></li>
+							<li><a href="https://www.linkedin.com/in/aur%C3%A8le-sarrail-7737b6153/">Linkedin</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+
+			<!-- Fin du contenu -->
+			<footer id="footer" class="page-footer font-small blue col-lg-12">
+	  			<div class="footer-copyright text-left py-3">
+	    			<ul id="nav_head" class=" list-unstyled list-inline">
+							<li><button  type="button" class="btn btn-link"><a href="#acceuil">Acceuil</a></button></li>
+							<li><button  type="button" class="btn btn-link"><a href="#Posts">Les Posts</a></button></li>
+							<li><button  type="button" class="btn btn-link"><a href="#contact">Contactez nous</a></button></li>
+					</ul>
+	 			 </div>
+			</footer>
+		</div>
+	</body>
+</div>
 </html>
