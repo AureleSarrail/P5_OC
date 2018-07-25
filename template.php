9a2d54d8b062d16375dcb54153d228995fec46f3
Aurele Sarrail <aurele.sarrail@gmail.com>
2018-07-25 13:54:30 +0200

    minor css modifications

--- template.php
+++ template.php
@@ -8,22 +8,22 @@
 </head>
 
 
-<body>
-	<div id="wrapper">
-		<div class="container-fluid" id="main">
-			<div id="header_global" class="row">
-				<header class="col-lg-12">
-					<div class="row">
-						<h1 class="col-lg-3" div id="logo">Blog AS-DEV</h1>
-						<ul id="nav_head" class=" list-unstyled list-inline col-lg-push-3 col-lg-6">
-							<li class="list-inline-item"><button  type="button" class="btn btn-link"><a href="#acceuil">Acceuil</a></button></li>
-							<li class="list-inline-item"><button  type="button" class="btn btn-link"><a href="#Posts">Les Posts</a></button></li>
-							<li class="list-inline-item"><button  type="button" class="btn btn-link"><a href="#contact">Contactez nous</a></button></li>
-						</ul>
-					</div>
-				</header>
-			</div>
-			<!-- Début du contenu à rendre MVC ensuite -->
+	<body>
+		<div id="wrapper">
+			<div class="container-fluid" id="main">
+				<div id="header_global" class="row">
+					<header class="col-lg-12">
+						<div class="row">
+							<h1 class="col-lg-3" div id="logo">Blog AS-DEV</h1>
+							<ul id="nav_head" class=" list-unstyled list-inline col-lg-push-3 col-lg-6">
+								<li class="list-inline-item"><button  type="button" class="btn btn-link"><a href="#acceuil">Acceuil</a></button></li>
+								<li class="list-inline-item"><button  type="button" class="btn btn-link"><a href="#Posts">Les Posts</a></button></li>
+								<li class="list-inline-item"><button  type="button" class="btn btn-link"><a href="#contact">Contactez nous</a></button></li>
+							</ul>
+						</div>
+					</header>
+				</div>
+				<!-- Début du contenu à rendre MVC ensuite -->
 
 			<div id="content_block">
 				<?= $viewContent ?>
@@ -31,16 +31,21 @@
 
 			<!-- Fin du contenu -->
 
+			</div>
 		</div>
-	</div>
+
 		<footer id="footer" class="footer col-lg-12">
-				<div class="footer-copyright text-left py-3">
-				<ul id="nav_head" class=" list-unstyled list-inline">
-						<li><button  type="button" class="btn btn-link"><a href="#acceuil">Acceuil</a></button></li>
-						<li><button  type="button" class="btn btn-link"><a href="#Posts">Les Posts</a></button></li>
-						<li><button  type="button" class="btn btn-link"><a href="#contact">Contactez nous</a></button></li>
-				</ul>
-				 </div>
+			<div class="row">
+				<div class="footer-copyright text-left py-3 col-lg-12">
+					<ul class=" list-unstyled list-inline footer_list">
+						<li class="col-lg-offset-1 col-lg-1"><a href="#acceuil">Acceuil</a></li>
+						<li class="col-lg-1"><a href="#Posts">Les Posts</a></li>
+						<li class="col-lg-1"><a href="#contact">Contactez nous</a></li>
+						<li class="col-lg-offset-7 col-lg-1"><a href="#connexion">Se connecter</a></li>
+					</ul>
+				</div>
+			</div>
 		</footer>
+
 	</body>
 </html>
