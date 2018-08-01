7d8134e3ccb97bcf921afe2062bc4e5fae3cae8a
Aurele Sarrail <aurele.sarrail@gmail.com>
2018-08-01 15:21:42 +0200

    some style change ti the display of posts and comments

--- Views/postsView.php
+++ Views/postsView.php
@@ -3,22 +3,20 @@
 
 <?php ob_start(); 
 
-foreach ($posts as $p) 
+foreach ($posts as $post) 
 {
 	?>
 
 	<div class="row">
-		<div class="col-lg-offset-3 col-lg-6">
-			<div class="row">
-				<img class="col-lg-3 img-responsive" src="<?= $p->getImage(); ?>" alt="image_post">
-				<h1 class="col-lg-9"><a href="index.php?action=onePost&amp;postId=<?= $p->getPostId() ?>"><?= $p->getTitle() ?></a></h1>
-				<h3 class="col-lg-12"><?= $p->getHead() ?></h3>
+		<div class="col-lg-offset-3 col-lg-6 post_global">
+				<img class="col-lg-3 img-responsive" src="<?= $post->getImage(); ?>" alt="image_post">
+				<h1 class="col-lg-9"><a href="index.php?action=onePost&amp;postId=<?= $post->getPostId() ?>"><?= $post->getTitle() ?></a></h1>
+				<h3 class="col-lg-12"><?= $post->getHead() ?></h3>
 				<div class="row">		
-					<p class="col-lg-12"><?= $p->getContent() ?></p>
-					<em><p class="col-lg-12">Créé par <strong><?= $p->getUsername() ?></strong> 
-						le <strong><?= substr($p->getCreatDate(),-2); ?>/<?= substr($p->getCreatDate(),5,2); ?>/<?= substr($p->getCreatDate(),0,4); ?></strong></p></em>
+					<p class="col-lg-12"><?= $post->getContent() ?></p>
+					<em><p class="col-lg-12">Créé par <strong><?= $post->getUsername() ?></strong> 
+						le <strong><?= substr($post->getCreatDate(),-2); ?>/<?= substr($post->getCreatDate(),5,2); ?>/<?= substr($post->getCreatDate(),0,4); ?></strong></p></em>
 				</div> 
-			</div>
 		</div>
 	</div>
 
