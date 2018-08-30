86358eef9e63610f5aded3a873f45b1c0468e26b
Aurele Sarrail <aurele.sarrail@gmail.com>
2018-08-30 17:54:29 +0200

    fixing code style #10

--- index.php
+++ index.php
@@ -43,7 +43,7 @@ if (isset($_GET['action'])) {
                 $_POST['name'],
                 $_POST['username'],
                 $_POST['mail'],
-                $_POST['password']);
+            $_POST['password']);
         }
     } elseif ($_GET['action'] == 'postCreated') {
         echo('ici');
@@ -63,7 +63,7 @@ if (isset($_GET['action'])) {
                     $_POST['content'],
                     $_SESSION['userId'],
                     $_SESSION['username'],
-                    $_SESSION['rights']);
+                $_SESSION['rights']);
             }
         }
     }
