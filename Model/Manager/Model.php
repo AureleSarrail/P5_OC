2a00caf98c574723771c704876ae3141bbd1eb11
Aurele Sarrail <aurele.sarrail@gmail.com>
2018-08-30 17:16:50 +0200

    fixing code style #7 - please be gentle !

--- Model/Manager/Model.php
+++ Model/Manager/Model.php
@@ -6,7 +6,11 @@ class Model
 {
     protected function dbConnect()
     {
-        $dataBase = new \PDO('mysql:host=localhost;dbname=Blog_asdev;charset=utf8', 'root', '', array(\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION));
+        $dataBase = new \PDO(
+        	'mysql:host=localhost;dbname=Blog_asdev;charset=utf8',
+        	'root',
+        	'',
+        	array(\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION));
         return $dataBase;
     }
 }
